4f6649cfaae2a5d5c8dec348f20560b80a7be4b2
Bambo0k <95869835+Bambo0k@users.noreply.github.com>
2022-07-28 21:14:33 +0300

    Bug Fixing

--- AJAX.js
+++ AJAX.js
@@ -1,13 +1,14 @@
 function fetchAnton(x) {
-  return new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", x);
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState !== 4 || xhr.status !== 200) {
-        return;
-      }
-      resolve(JSON.parse(xhr.response));
-    };
-    xhr.send();
-  });
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", x);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState !== 4 || xhr.status !== 200) {
+                return;
+            }
+            resolve(JSON.parse(xhr.response));
+        };
+        xhr.send();
+    });
 }
+module.exports = fetchAnton;
